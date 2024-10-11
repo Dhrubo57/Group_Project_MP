893240132cbacd44cf16d02b069695d29fa431db
Dhrubo57 <156521532+Dhrubo57@users.noreply.github.com>
2024-10-11 10:04:13 +0600

    Add files via upload

---
+++ MoversAndPackers/moverspackers/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+# Create your views here.
+from .models import *
+def index(request):
+    return render(request, 'index.html')
+def admin_login(request):
+    error = ""
+    if request.method == 'POST':
+        u = request.POST['username']
+        p = request.POST['pwd']
+        user = authenticate(username=u, password=p)
+        try:
+            if user.is_staff:
+                login(request, user)
+                error = "no"
+            else:
+                error = "no"
+        except:
+            error="yes"
+    return render(request, 'admin_login.html', locals())
+
+def admin_home(request):
+    if not request.user.is_authenticated:
+        return redirect('admin_login')
+    return render(request, 'admin_home.html')
+
+def Logout(request):
+    logout(request)
+    return redirect(index)
+
+def add_services(request):
+    if not request.user.is_authenticated:
+        return redirect('admin_login')
+    error =""
+    if request.method=="POST":
+        st = request.POST['servicetitle']
+        des = request.POST['description']
+        image = request.FILES['image']
+        try:
+            Services.objects.create(title=st,description=des,image=image)
+            error="no"
+        except:
+            error = "yes"
+    return render(request, 'add_services.html',locals())
+
+def manage_services(request):
+    if not request.user.is_authenticated:
+        return redirect('admin_login')
+    services = Services.objects.all()
+    return render(request, 'manage_services.html',locals())
+
+def edit_service(request,pid):
+    if not request.user.is_authenticated:
+        return redirect('admin_login')
+    service = Services.objects.get(id=pid)
+    error =""
+    if request.method=="POST":
+        st = request.POST['servicetitle']
+        des = request.POST['description']
+
+        service.title = st
+        service.description = des
+
+        try:
+            service.save()
+            error="no"
+        except:
+            error = "yes"
+        try:
+          image = request.FILES['image']
+          service.image = image
+          service.save()
+        except:
+          pass
+    return render(request, 'edit_service.html',locals())
+
